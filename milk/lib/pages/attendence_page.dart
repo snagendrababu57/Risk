92a257a292cbe0b7ab5805dcdd2c791791b93afa
snagendrababu57 <snagendrababu57@gmail.com>
2025-04-06 18:08:58 +0530

    Update attendence page

--- milk/lib/pages/attendence_page.dart
+++ milk/lib/pages/attendence_page.dart
@@ -14,6 +14,7 @@ class AttendancePage extends StatefulWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
   _AttendancePageState createState() => _AttendancePageState();
 }
 
